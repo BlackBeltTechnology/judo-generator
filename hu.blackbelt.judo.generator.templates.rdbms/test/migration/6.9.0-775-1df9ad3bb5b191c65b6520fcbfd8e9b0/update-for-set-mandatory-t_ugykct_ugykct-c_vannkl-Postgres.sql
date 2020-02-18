cb097c1750b31293e7ff8bdd1be462ad07ef50a0
Gyula Turi <gyula.turi@blackbelt.hu>
2020-02-18 17:49:29 +0100

    schema migration scripts fixed

---
+++ hu.blackbelt.judo.generator.templates.rdbms/test/migration/6.9.0-775-1df9ad3bb5b191c65b6520fcbfd8e9b0/update-for-set-mandatory-t_ugykct_ugykct-c_vannkl-Postgres.sql
@@ -0,0 +1,2 @@
+--changeset manual:UPDATE_TABLE_[%= table.sqlName %]_[%=field.sqlName %]#[%= version %] logicalFilePath:[%= scriptName %] stripComments:true
+UPDATE UPDATE T_UGYKCT_UGYKCT SET C_VANNKL = 'N';
